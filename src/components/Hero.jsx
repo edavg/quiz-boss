026c5aed730b1485874b9ea7747817b92d031001
Edavg <eduardo.avg95@gmail.com>
2023-11-16 10:55:12 -0300

    7mo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <div className='w-[65%] mx-auto text-center text-white'>
             <h1 className='text-2xl md:text-5xl mt-6 font-bold'> ¿Cuánto sabés acerca del Reglamento <br/> General de Tránsito? </h1>
             <p className=' md:text-3xl my-6'>Poné a prueba tus conocimientos en este juego</p>
-            <Link to={'/quiz'}><button className=' w-56 py-4 bg-red-600 rounded-lg font-bold text-2xl md:text-4xl hover:bg-red-400'>Comenzar</button></Link>
+            <Link to={'/quiz'}><button className='w-36 md:w-56 py-2 md:py-4 bg-red-600 rounded-lg font-bold text-2xl md:text-4xl hover:bg-red-400'>Comenzar</button></Link>
         </div>
       </div>
     </div>
